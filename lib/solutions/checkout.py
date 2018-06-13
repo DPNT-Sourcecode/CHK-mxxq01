e828fe27737726ee6fa320d275b505bfa97245d6
sbx_user1059 <sbx_user1059@ip-10-12-56-238.eu-west-2.compute.internal>
2018-06-13 15:23:42 +0000

    Wed Jun 13 15:23:42 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,4 +1,11 @@
-
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
