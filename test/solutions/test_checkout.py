4c450f51c2fd786322ad8ea499be73c9d213b644
sbx_user1059 <sbx_user1059@ip-10-11-5-102.eu-west-2.compute.internal>
2018-06-13 16:23:28 +0000

    Wed Jun 13 16:23:28 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -9,7 +9,8 @@ class TestSum(unittest.TestCase):
         self.assertEqual(checkout("ABCCCD"), 50+30+20*3+15)
         self.assertEqual(checkout("AAA"), 130)
         self.assertEqual(checkout("AAAAB"), 130+50+30)
-        self.assertEqual(checkout("AAAAAB"), 130+200+30)
+        self.assertEqual(checkout("AAAAAAAAB"), 130+200+30)
+        self.assertEqual(checkout("EEBB"), 40*2+30)
 
 
 if __name__ == '__main__':
