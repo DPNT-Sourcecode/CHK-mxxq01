f42cb7729cfa38511c219508ca8eea3fc8324797
sbx_user1059 <sbx_user1059@ip-10-11-5-102.eu-west-2.compute.internal>
2018-06-13 16:33:45 +0000

    Wed Jun 13 16:33:45 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -9,6 +9,26 @@ from collections import defaultdict
 # | D    | 15    |                        |
 # | E    | 40    | 2E get one B free      |
 # | F    | 10    | 2F get one F free      |
+# | G    | 20    |                        |
+# | H    | 10    | 5H for 45, 10H for 80  |
+# | I    | 35    |                        |
+# | J    | 60    |                        |
+# | K    | 80    | 2K for 150             |
+# | L    | 90    |                        |
+# | M    | 15    |                        |
+# | N    | 40    | 3N get one M free      |
+# | O    | 10    |                        |
+# | P    | 50    | 5P for 200             |
+# | Q    | 30    | 3Q for 80              |
+# | R    | 50    | 3R get one Q free      |
+# | S    | 30    |                        |
+# | T    | 20    |                        |
+# | U    | 40    | 3U get one U free      |
+# | V    | 50    | 2V for 90, 3V for 130  |
+# | W    | 20    |                        |
+# | X    | 90    |                        |
+# | Y    | 10    |                        |
+# | Z    | 50    |                        |
 # +------+-------+------------------------+
 
 sku_prices = {
@@ -18,6 +38,26 @@ sku_prices = {
     'D': 15,
     'E': 40,
     'F': 10,
+    'G': 20,
+    'H': 10,
+    'I': 35,
+    'J': 60,
+    'K': 80,
+    'L': 90,
+    'M': 15,
+    'N': 40,
+    'O': 10,
+    'P': 50,
+    'Q': 30,
+    'R': 50,
+    'S': 30,
+    'T': 20,
+    'U': 40,
+    'V': 50,
+    'W': 20,
+    'X': 90,
+    'Y': 10,
+    'Z': 50,
 }
 
 def __multiple_promo(basket, sku, count, price):
@@ -49,7 +89,7 @@ def promo_f_ffree(basket):
     f_count = basket['F']
     offers = f_count / 3
     if offers > 0:
-        basket['F'] -= offers
+        basket['F'] = max(0, basket['F'] - offers)
     return basket, 0
 
 promotions = [
