77a1e9dbb4fdc2561bf8e4f242a04a5410b8c871
sbx_user1059 <sbx_user1059@ip-10-11-5-102.eu-west-2.compute.internal>
2018-06-13 16:02:01 +0000

    Wed Jun 13 16:02:01 UTC 2018

--- test/solutions/test_checkout.py
+++ test/solutions/test_checkout.py
@@ -6,6 +6,9 @@ from lib.solutions.checkout import checkout
 class TestSum(unittest.TestCase):
     def test_checkout(self):
         self.assertEqual(checkout("ABC"), 100)
+        self.assertEqual(checkout("ABCCCD"), 155)
+        self.assertEqual(checkout("AAA"), 130)
+        self.assertEqual(checkout("AAAAB"), 210)
 
 
 if __name__ == '__main__':
