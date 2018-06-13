0a3c0a59b316fe69ee9e3be37ebe5e7d354f83b6
sbx_user1059 <sbx_user1059@ip-10-12-56-238.eu-west-2.compute.internal>
2018-06-13 15:26:42 +0000

    Wed Jun 13 15:26:42 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -7,6 +7,24 @@
 # | D    | 15    |                |
 # +------+-------+----------------+
 
+sku_prices = {
+    'A': 50,
+    'B': 30,
+    'C': 20,
+    'D': 15,
+}
+
+promotions = {
+    'A': {
+        'quantity': 3,
+        'price': 130,
+    },
+    'B': {
+        'quantity': 2,
+        'price': 45,
+    }
+}
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
