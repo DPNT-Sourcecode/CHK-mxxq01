f27c2c458ff1e938dc8c23b82794b3f6e5315677
sbx_user1059 <sbx_user1059@ip-10-11-5-102.eu-west-2.compute.internal>
2018-06-13 16:45:41 +0000

    Wed Jun 13 16:45:41 UTC 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,36 +1,5 @@
 from collections import defaultdict
 
-# +------+-------+------------------------+
-# | Item | Price | Special offers         |
-# +------+-------+------------------------+
-# | A    | 50    | 3A for 130, 5A for 200 |
-# | B    | 30    | 2B for 45              |
-# | C    | 20    |                        |
-# | D    | 15    |                        |
-# | E    | 40    | 2E get one B free      |
-# | F    | 10    | 2F get one F free      |
-# | G    | 20    |                        |
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | I    | 35    |                        |
-# | J    | 60    |                        |
-# | K    | 80    | 2K for 150             |
-# | L    | 90    |                        |
-# | M    | 15    |                        |
-# | N    | 40    | 3N get one M free      |
-# | O    | 10    |                        |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | R    | 50    | 3R get one Q free      |
-# | S    | 30    |                        |
-# | T    | 20    |                        |
-# | U    | 40    | 3U get one U free      |
-# | V    | 50    | 2V for 90, 3V for 130  |
-# | W    | 20    |                        |
-# | X    | 90    |                        |
-# | Y    | 10    |                        |
-# | Z    | 50    |                        |
-# +------+-------+------------------------+
-
 sku_prices = {
     'A': 50,
     'B': 30,
@@ -42,7 +11,7 @@ sku_prices = {
     'H': 10,
     'I': 35,
     'J': 60,
-    'K': 80,
+    'K': 70,
     'L': 90,
     'M': 15,
     'N': 40,
@@ -50,14 +19,14 @@ sku_prices = {
     'P': 50,
     'Q': 30,
     'R': 50,
-    'S': 30,
+    'S': 20,
     'T': 20,
     'U': 40,
     'V': 50,
     'W': 20,
-    'X': 90,
-    'Y': 10,
-    'Z': 50,
+    'X': 17,
+    'Y': 20,
+    'Z': 21,
 }
 
 def __multiple_promo(basket, sku, count, price):
@@ -78,11 +47,23 @@ def __get_some_free(basket, buy_sku, buy_count, free_sku):
         basket[free_sku] = max(0, basket[free_sku] - offers)
     return basket, 0
 
-# | H    | 10    | 5H for 45, 10H for 80  |
-# | K    | 80    | 2K for 150             |
-# | P    | 50    | 5P for 200             |
-# | Q    | 30    | 3Q for 80              |
-# | V    | 50    | 2V for 90, 3V for 130  |
+# | A    | 50    | 3A for 130, 5A for 200          |
+# | B    | 30    | 2B for 45                       |
+# | E    | 40    | 2E get one B free               |
+# | F    | 10    | 2F get one F free               |
+# | H    | 10    | 5H for 45, 10H for 80           |
+# | K    | 70    | 2K for 120                      |
+# | N    | 40    | 3N get one M free               |
+# | P    | 50    | 5P for 200                      |
+# | Q    | 30    | 3Q for 80                       |
+# | R    | 50    | 3R get one Q free               |
+# | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | U    | 40    | 3U get one U free               |
+# | V    | 50    | 2V for 90, 3V for 130           |
+# | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+# | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
 
 promotions = [
     lambda b: __get_some_free(b, 'E', 2, 'B'),
